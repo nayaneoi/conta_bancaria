3a3e0d39ea6e2d59cef7263e0f8def2d7ccba94b
Nayane Oi <nayane_rms@hotmail.com>
2025-11-05 17:06:49 -0300

    Iniciando o projeto e add Menu

---
+++ Menu.ts
@@ -0,0 +1,113 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/Colors";
+
+export function main(){
+
+    let opcao: number;
+
+    while(true){
+
+        console.log(colors.bg.black, colors.fg.yellow,
+                    "***********************************************************");
+        console.log("                                                           ");
+        console.log("                 Banco do Brazil com Z                     ");
+        console.log("                                                           ");
+        console.log("***********************************************************");
+        console.log("                                                           ");
+        console.log("              1 - Criar Conta                              ");
+        console.log("              2 - Listar todas as Contas                   ");
+        console.log("              3 - Buscar Conta por Numero                  ");
+        console.log("              4 - Atualizar Dados da Conta                 ");
+        console.log("              5 - Apagar Conta                             ");
+        console.log("              6 - Sacar                                    ");
+        console.log("              7 - Depositar                                ");
+        console.log("              8 - Transferir valores entre Contas          ");
+        console.log("              9 - Sair                                     ");
+        console.log("                                                           ");
+        console.log("***********************************************************");
+        console.log("                                                           ",
+        colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong,
+                "\nBanco do Brazil com z - O seu Futuro começa aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao){
+            case 1:
+                console.log(colors.fg.whitestrong,
+                    "\n\nCriar Conta\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong,
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+                
+                KeyPress()
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong,
+                    "\n\nBuscar Conta por Numero\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong,
+                    "\n\nAtualizar Dados da Conta\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong,
+                    "\n\nApagar Conta\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 6:
+                console.log(colors.fg.whitestrong,
+                    "\n\nSacar\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 7:
+                console.log(colors.fg.whitestrong,
+                    "\n\nDepositar\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            case 8:
+                console.log(colors.fg.whitestrong,
+                    "\n\nTransferir valores entre Contas\n\n", colors.reset);
+
+                KeyPress()
+                break;
+            default:
+                console.log(colors.fg.whitestrong,
+                    "\n\nOpção Inválida!\n\n", colors.reset);
+
+                KeyPress()
+                break;
+        }
+    }
+}
+
+export function sobre(): void {
+    console.log("\n***********************************************************");
+    console.log("Projeto Desenvolvido por: Nayane Rodrigues Matos Santos Oi   ");
+    console.log("nayane_rms@hotmail.com                                       ");
+    console.log("https://github.com/nayaneoi                                  ");
+    console.log("*************************************************************");
+}
+main();
+function KeyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
